23fb2e306786b0fbdf9cc6f4e029511cb56165f7
Rohandalal8 <rohandalal079@gmail.com>
2026-08-25 19:57:24 +0530

    Variables and Data Types complete

---
+++ VariablesAndDataTypes.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println(a + " + " + b + " = " + (a + b));
+
+        // String name = sc.nextLine();
+        // System.out.println("Hello, " + name + "!");
+
+        // float rad = sc.nextFloat();
+        // float area = 3.14f * rad * rad;
+        // System.out.println("Area of circle with radius " + rad + " is: " + area);
+
+        // float a = 25.12f;
+        // int b = (int) a; // Type casting from float to int
+        // System.out.println("Int value: " + b);
+
+        char ch = 'A';
+        int num = ch; // Implicit type casting from char to int
+        System.out.println("ASCII value of " + ch + " is: " + num);
+    }
+}
