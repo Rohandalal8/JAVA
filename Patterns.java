d2089c45f1816d0e3a98b5d698a9b1b4a424e062
Rohandalal8 <rohandalal079@gmail.com>
2026-08-31 21:46:12 +0530

    Patterns completed

--- Patterns.java
+++ Patterns.java
@@ -2,28 +2,28 @@ public class Patterns {
     public static void main(String[] args) {
 
         // star pattern
-        // for (int i = 1; i <= 5; i++) {
-            // for (int j = 1; j <= i; j++) {
-                // System.out.print("* ");
-            // }
-            // System.out.println();
-        // }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // inverted star pattern
-        // for (int i = 5; i >= 1; i--) {
-            // for (int j = 1; j <= i; j++) {
-                // System.out.print("* ");
-            // }
-            // System.out.println();
-        // }
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
 
         // half pyramid pattern
-        // for (int i = 1; i <= 5; i++) {
-        //     for (int j = 1; j <= i; j++) {
-        //         System.out.print(j + " ");
-        //     }
-        //     System.out.println();
-        // }
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
 
         // character pattern
         char ch = 'A';
