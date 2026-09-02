d8b5322167e63ea0c6a99866f2b86668a3aa21bc
Rohandalal8 <rohandalal079@gmail.com>
2026-09-02 12:12:28 +0530

    functions

---
+++ FunctionsAndMethods.java
@@ -0,0 +1,8 @@
+public class FunctionsAndMethods {
+    public static void printHello() {
+        System.out.println("Hello, World!");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+}
