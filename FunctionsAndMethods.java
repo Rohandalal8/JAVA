57cf1436dafb8544648a5ceb9fe62477619108c9
Rohandalal8 <rohandalal079@gmail.com>
2026-09-03 19:53:53 +0530

    functions

--- FunctionsAndMethods.java
+++ FunctionsAndMethods.java
@@ -1,8 +1,23 @@
+import java.util.*;
+
 public class FunctionsAndMethods {
+
     public static void printHello() {
         System.out.println("Hello, World!");
     }
+
+    public static int addNumbers(int x, int y) {
+        int sum = x + y;
+        return sum;
+    }
+
     public static void main(String[] args) {
-        printHello();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = addNumbers(a, b);
+        System.out.println("Sum: " + sum);
+
+        // printHello();
     }
 }
